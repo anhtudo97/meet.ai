ff49d6103ceb582debbffafa32d1778a77555d8b
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-11-04 00:10:44 +0700

    feat: setting db auth

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+let authInstance: ReturnType<typeof betterAuth> | null = null;
+
+export const getAuth = async () => {
+  if (authInstance) return authInstance;
+
+  authInstance = betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg", // or "sqlite" or "mysql"
+    }),
+    secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL,
+    emailAndPassword: {
+      enabled: true,
+      disableSignUp: false,
+      requireEmailVerification: false,
+      minPasswordLength: 8,
+      maxPasswordLength: 128,
+      autoSignIn: true,
+    },
+    plugins: [nextCookies()],
+  });
+
+  return authInstance;
+};
+
+export const auth = await getAuth();
