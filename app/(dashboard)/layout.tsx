d99dddf80fa70df173af801c655ecc14e9eb1018
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-11-12 23:28:03 +0700

    feat: config index page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboard-sidebar';
+import { PropsWithChildren } from 'react';
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className='flex flex-col h-screen w-screen bg-muted'>
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
