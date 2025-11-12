7cfef3c6c393df8813fd40690b9d8289ae9b10f6
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-11-12 23:28:28 +0700

    feat: generated avatar

---
+++ components/generated-avatar.tsx
@@ -0,0 +1,43 @@
+import { botttsNeutral, initials } from '@dicebear/collection';
+import { createAvatar } from '@dicebear/core';
+import { Avatar } from '@radix-ui/react-avatar';
+import { AvatarFallback, AvatarImage } from './ui/avatar';
+
+interface GeneratedAvatarProps {
+  // Define any props if needed
+  seed: string;
+  className?: string;
+  variant?: "botttsNeutral" | "initials";
+}
+
+
+const GeneratedAvatar = ({ seed, className, variant }: GeneratedAvatarProps) => {
+  let avatar;
+
+  if (variant === "botttsNeutral") {
+    avatar = createAvatar(
+      botttsNeutral,
+      {
+        seed
+      }
+    );
+  } else {
+    avatar = createAvatar(
+      initials,
+      {
+        seed: seed,
+        size: 40,
+        radius: 50,
+      }
+    );
+  }
+
+  return (
+    <Avatar className={className}>
+      <AvatarImage src={avatar.toDataUri()} alt="Generated Avatar" />
+      <AvatarFallback>{seed.charAt(0).toUpperCase()}</AvatarFallback>
+    </Avatar>
+  );
+};
+
+export default GeneratedAvatar;
