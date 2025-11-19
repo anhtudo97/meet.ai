adf2c4cbcce3368f337439c98e7e25ac899c49a9
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-11-20 00:14:05 +0700

    feat: agents setup

--- modules/agents/server/procedures.ts
+++ modules/agents/server/procedures.ts
@@ -5,9 +5,6 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 export const agentsRouter = createTRPCRouter({
   getMany: baseProcedure.query(async ({ ctx }) => {
     const data = await db.select().from(agents);
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     return data;
   })
 });
