16b769caee62d21061765c66178a9eea613f823f
Alex Dou <38291962+anhtudo97@users.noreply.github.com>
2025-12-09 02:22:14 +0700

    feat: meeting detail page

--- app/(dashboard)/meetings/[meetingId]/page.tsx
+++ app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,5 +1,31 @@
-export const Page = () => {
-  return <div>Meeting ID</div>
+import {
+  MeetingDetailView,
+  MeetingsDetailViewError,
+  MeetingsDetailViewLoading
+} from "@/modules/meetings/ui/views/meeting-detail-view"
+import { getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+
+interface AgentPageProps {
+  params: Promise<{ agentId: string }>
+}
+
+export const Page = async ({ params }: AgentPageProps) => {
+  const { agentId } = await params
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.agents.getOne.queryOptions({ id: agentId }))
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsDetailViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsDetailViewError />}>
+          <MeetingDetailView meetingId={agentId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  )
 }
 
 export default Page
